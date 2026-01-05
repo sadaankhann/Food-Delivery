eacb1806d32a99f6c39a5680f8f73f7ce22bc4fa
Sadaan Khan Durrani <sadaankhan2346@gmail.com>
2026-01-06 03:08:46 +0500

    Third C

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -42,8 +42,6 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
 
     res.json({ success: true, session_url: session.url });
